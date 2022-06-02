5515eccf7c09209f43b4f6c3b3fc678cdb042a64
asiful087 <asifulhaque087@gmail.com>
2022-06-02 13:19:58 +0600

    add background transparent

---
+++ lua/user/nvim-transparent.lua
@@ -0,0 +1,26 @@
+local status_ok, nvim_tranparent = pcall(require, "transparent")
+if not status_ok then
+  return
+end
+
+
+
+
+
+nvim_tranparent.setup {
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+
+    -- example of akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+}
+
+
